2c5a099b26f1f0a53dc7d318ed729e90c051a6b4
Garvitmaheshwari2001 <86588690+Garvitmaheshwari2001@users.noreply.github.com>
2022-06-21 20:12:14 +0530

    Add files via upload

---
+++ Heap/Kthlargest.cpp
@@ -0,0 +1,12 @@
+int kthLargest(vector<int>& arr, int size, int K)
+{
+	    priority_queue<int,vector<int>,greater<int>> pq;
+    int cnt =1;
+    for(int i=0;i<size;i++){
+        pq.push(arr[i]);
+        if(cnt>K) pq.pop();
+        cnt++;
+    }
+//     cout<<pq.size();
+    return pq.top();
+}
